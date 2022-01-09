18339df0c6be146461db31504ce1f20f181bfd9a
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2022-01-09 16:06:09 -0600

    2

--- learning.py
+++ learning.py
@@ -6,5 +6,19 @@ print('hello Angie')
 angie = 'angie is awesome'
 print(angie)
 
+
+elevator_up = False
+
+def move(direction):
+    if (direction):
+        print('do_it')
+    else:
+        print("don't do it")
+
+
+def add_text(first, second):
+    return first + second
+
+
 if __name__ == '__main__':
     print(angie)
